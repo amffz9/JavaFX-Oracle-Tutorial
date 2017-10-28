2bca4507f8dbc743c295e047b05c08e6f0ed0cfe
Adam Forbis <aforbis83091@gmail.com>
2017-10-28 14:53:40 -0500

    Made a copy of the login form to use in the styling exercise. Then used the css to set a background image.

---
+++ src/sample/FancyForm.java
@@ -0,0 +1,89 @@
+/*
+ * This file is following part 3 of the
+ * Oracle JavaFX tutorial, this is a continuation of part two
+ * and adds in styling the form with css
+ * some comments are copied from the
+ * tutorial page instructions
+ */
+
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class FancyForm extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Welcome");
+
+        /*
+         * For the login form, use a GridPane layout because it enables you to create a flexible grid
+         * of rows and columns in which to lay out controls. You can place controls in any cell in the grid,
+         * and you can make controls span cells as needed.
+         */
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        /*
+         * Create the text fields their labels and add them to the grid
+         */
+        final Text actionTarget = new Text();
+        grid.add(actionTarget, 1, 6);
+
+        Text sceneTitle = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0,0,2,1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign In");
+        btn.setOnAction(event -> {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Sign in button pressed");
+        });
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        grid.setGridLinesVisible(true);
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add(
+                FancyForm.class.getResource("fancyform.css").toExternalForm()
+        );
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+
